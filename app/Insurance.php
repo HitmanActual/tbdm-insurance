335e3b7b49c6f4712b2dbd7d958ee552dc05558e
HitmanActual <dev.render@gmail.com>
2020-05-18 01:20:51 +0200

    deleted at and handler

--- app/Insurance.php
+++ app/Insurance.php
@@ -4,11 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Insurance extends Model
 {
     
 
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
